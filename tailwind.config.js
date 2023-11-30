efd10f781fe1db2a1ab0224108efa633f388c6b6
Tomas Pereira <tomaspereira2k@gmail.com>
2023-11-30 05:25:04 -0300

    completed themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,46 +26,55 @@ export default {
           textColor: {
             "key-shadow": "hsl(224, 28%, 35%)",
             "key-shadow-2": "hsl(28, 16%, 65%)",
-            "text": "hsl(221, 14%, 31%)",
+            "text-1": "hsl(221, 14%, 31%)",
+            "text-2": "hsl(0, 0%, 100%)"
           },
           backgroundColor: {
             "main": "hsl(222, 26%, 31%)",
             "toggle-bg": "hsl(223, 31%, 20%)",
             "toggle-btn": "hsl(6, 63%, 50%)",
             "screen": "hsl(224, 36%, 15%)",
-            "key-bg-1": "hsl(225, 21%, 49%)",
-            "key-bg-2": "hsl(30, 25%, 89%)",
+            "key-bg-1": "hsl(30, 25%, 89%)",
+            "key-bg-2": "hsl(225, 21%, 49%)",
+            "key-bg-3": "hsl(6, 63%, 50%)"
           }
         }
       },
       themes: [
         {
           name: "theme-2",
+          selectors:['[data-theme="theme-2"]'],
           extend: {
+            borderColor: {
+              "key-shadow": "hsl(185, 58%, 25%)",
+              "key-shadow-2": "hsl(35, 11%, 61%)",
+              "key-shadow-3": "hsl(25, 99%, 27%)"
+            },
             textColor: {
-              "key-shadow-1": "hsl(185, 58%, 25%)",
-              "key-shadow-2": "hsl(25, 99%, 27%)",
-              "key-shadow-3": "hsl(35, 11%, 61%)",
-              "text": "hsl(60, 10%, 19%)"
+              "text-1": "hsl(60, 10%, 19%)",
+              "text-2": "hsl(0, 0%, 100%)"
             },
             backgroundColor: {
               "main": "hsl(0, 0%, 90%)",
               "toggle": "hsl(0, 5%, 81%)",
               "toggle-btn": "hsl(25, 98%, 40%)",
               "screen": "hsl(0, 0%, 93%)",
-              "key-bg-1": "hsl(185, 42%, 37%)",
-              "key-bg-2": "hsl(25, 98%, 40%)",
-              "key-bg-3": "hsl(45, 7%, 89%)"
+              "key-bg-1": "hsl(45, 7%, 89%)",
+              "key-bg-2": "hsl(185, 42%, 37%)",
+              "key-bg-3": "hsl(25, 98%, 40%)"
             },
           }
         },
         {
           name: "theme-3",
+          selectors:['[data-theme="theme-3"]'],
           extend: {
+            borderColor: {
+              "key-shadow": "hsl(285, 91%, 52%)",
+              "key-shadow-2": "hsl(290, 70%, 36%)",
+              "key-shadow-3": "hsl(177, 92%, 70%)"
+            },
             textColor: {
-              "key-shadow-1": "hsl(285, 91%, 52%)",
-              "key-shadow-2": "hsl(177, 92%, 70%)",
-              "key-shadow-3": "hsl(290, 70%, 36%)",
               "text-1": "hsl(52, 100%, 62%)",
               "text-2": "hsl(198, 20%, 13%)"
             },
@@ -73,9 +82,9 @@ export default {
               "main": "hsl(268, 75%, 9%)",
               "toggle": "hsl(268, 71%, 12%)",
               "toggle-btn": "hsl(176, 100%, 44%)",
-              "key-bg-1": "hsl(281, 89%, 26%)",
-              "key-bg-2": "hsl(176, 100%, 44%)",
-              "key-bg-3": "hsl(268, 47%, 21%)",
+              "key-bg-1": "hsl(268, 47%, 21%)",
+              "key-bg-2": "hsl(281, 89%, 26%)",
+              "key-bg-3": "hsl(176, 100%, 44%)",
             }
           }
         }
--- theme-swticher.js
+++ theme-swticher.js
@@ -2,60 +2,24 @@ const themeSwitcher = document.querySelector(".theme-switcher");
 const body = document.querySelector("body");
 const divs = document.querySelectorAll("div");
 const buttons = document.querySelectorAll("button");
-
-
+const calculator = document.querySelector(".calculator");
 
 
 themeSwitcher.addEventListener("click", (e) => {
-    if(e.target.matches === "input"){
-        const button = e.target;
-        const theme = button.dataset.theme;
+    if(e.target.matches("input")){
+        const themeValue = e.target.value;
 
-        secondThemeBtn.addEventListener("click", () => {
+        if(themeValue === "default-theme"){
+            body.dataset.theme = themeValue;
+        }
 
-            body.setAttribute("class", "theme-2")
-        
-            for(let i = 0; i < divs.length; i++){
-                divs[i].classList.toggle("theme-2")
-            }
-        
-            for(let j = 0; j < buttons.length; j++){
-                buttons[j].classList.toggle("theme-2")
-            }
-        
-            button.dataset.theme = "theme-2";
-        
-        })
-        
-        defaultThemeBtn.addEventListener("click", () => {
-        
-        
-            body.classList.toggle("defaultTheme");
-        
-            for(let i = 0; i < divs.length; i++){
-                divs[i].classList.toggle("defaultTheme")
-            }
-        
-            for(let j = 0; j < buttons.length; j++){
-                buttons[j].classList.toggle("defaultTheme")
-            }
-        
-            button.dataset.theme = "default";
-        })
-        
-        thirdThemeBtn.addEventListener("click", () => {
-        
-            body.classList.toggle("theme-3");
-        
-            for(let i = 0; i < divs.length; i++){
-                divs[i].classList.toggle("theme-3")
-            }
-        
-            for(let j = 0; j < buttons.length; j++){
-                buttons[j].classList.toggle("theme-3")
-            }
-        
-            button.dataset.theme = "theme-3";
-        })
+        if(themeValue === "theme-2"){
+            body.dataset.theme = themeValue;
+        }
+
+        if(themeValue === "theme-3"){
+            body.dataset.theme = themeValue;
+        }
     }
+    
 })
